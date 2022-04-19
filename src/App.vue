a55c873dfd261fe2e0d2eea961565fdbd61fc1c4
grasulkhuja <rasulganiyev1999@gmail.com>
2022-04-19 17:53:49 +0500

    imported invoice calculator component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app>
-    <v-main> </v-main>
+    <app-bar />
+    <v-main>
+      <invoice-calculator />
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
+import AppBar from '@/components/app/AppBar.vue'
+import InvoiceCalculator from '@/components/InvoiceCalculator.vue'
 export default Vue.extend({
   name: 'App',
+  components: { InvoiceCalculator, AppBar },
 })
 </script>
