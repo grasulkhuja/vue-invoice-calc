669b6b1adae59435b7d325eb1acb32372c095d4b
GRasul99 <rasulganiyev1999@gmail.com>
2022-04-20 07:47:26 +0500

    added validation for creating new product

--- src/components/InvoiceCalculator.vue
+++ src/components/InvoiceCalculator.vue
@@ -5,40 +5,80 @@
         <v-card>
           <v-card-title>Vue Invoice Calculator</v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="product.name"
-              dense
-              outlined
-              label="Product name"
-              hint="Enter a product name"
-            />
-            <v-text-field
-              v-model="product.price"
-              dense
-              outlined
-              label="Product price"
-              hint="Enter a product price"
-            />
-            <v-text-field
-              v-model="product.quantity"
-              dense
-              outlined
-              label="Product quantity"
-              hint="Enter a product quantity"
-            />
-            <v-spacer />
-            <v-btn outlined @click.prevent="addProduct">Add</v-btn>
+            <validation-observer ref="isProductValid" v-slot="{ invalid }">
+              <v-form @submit.prevent="addProduct">
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <validation-provider name="Product name" v-slot="{ errors }" rules="required">
+                      <v-text-field
+                        v-model="product.name"
+                        dense
+                        outlined
+                        label="Product name"
+                        hint="Enter a product name"
+                        :hide-details="!errors"
+                        :error-messages="errors[0]"
+                      />
+                    </validation-provider>
+                  </v-col>
+                  <v-col cols="12" md="2">
+                    <validation-provider name="Price" rules="required" v-slot="{ errors }">
+                      <v-text-field
+                        v-model="product.price"
+                        :hide-details="!errors"
+                        :error-messages="errors[0]"
+                        dense
+                        outlined
+                        label="Price"
+                        hint="Enter a product price"
+                      />
+                    </validation-provider>
+                  </v-col>
+                  <v-col cols="12" md="2">
+                    <validation-provider
+                      name="Quantity"
+                      rules="required|numeric"
+                      v-slot="{ errors }"
+                    >
+                      <v-text-field
+                        v-model="product.quantity"
+                        dense
+                        outlined
+                        label="Quantity"
+                        hint="Enter a product quantity"
+                        :hide-details="!errors"
+                        :error-messages="errors[0]"
+                      />
+                    </validation-provider>
+                  </v-col>
+                  <v-col cols="12" md="1" class="ml-auto mr-4">
+                    <v-btn outlined type="submit" @click.prevent="addProduct" :disabled="invalid">
+                      Add
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </validation-observer>
+
             <v-data-table
+              v-model="selectedProducts"
               :headers="productTableHeaders"
               :items="products"
               item-key="id"
               disable-pagination
               hide-default-footer
-            />
+              show-select
+            >
+              <template v-slot:[`item.summary`]="{ item }">
+                {{ summaryForEachProduct(item) }}
+              </template>
+            </v-data-table>
             <v-card-actions>
-              <v-btn outlined>Delete</v-btn>
+              <v-btn outlined :disabled="!selectedProducts.length">Delete</v-btn>
               <v-spacer />
-              Total: {{ total }}
+              <p>
+                Total: $ <span>{{ total ? total : 0 }}</span>
+              </p>
             </v-card-actions>
           </v-card-text>
         </v-card>
@@ -48,38 +88,66 @@
 </template>
 
 <script lang="ts">
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import { Product } from '@/types/product'
+import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+import { required, numeric } from 'vee-validate/dist/rules'
 import Vue from 'vue'
+
+setInteractionMode('eager')
+
+extend('required', {
+  ...required,
+  message: '{_field_} can not be empty',
+})
+extend('numeric', {
+  ...numeric,
+  message: '{_field_} can contain only numbers',
+})
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    fetchAllProducts: () => void
+  }
+}
+
 export default Vue.extend({
   name: 'InvoiceCalculator',
+  data() {
+    return {
+      selectedProducts: [] as Product[],
+    }
+  },
   methods: {
+    async fetchAllProducts() {
+      await this.$store.dispatch('getAllProducts')
+    },
     async addProduct(): Promise<void> {
       await this.$store.dispatch('addProduct', this.product)
       await this.$store.dispatch('clearProduct')
     },
+    summaryForEachProduct(product: Product): number {
+      return product.quantity * product.price
+    },
   },
   computed: {
-    product: {
-      get(): Product {
-        return {
-          id: this.$store.state.products.length + 1,
-          name: '',
-          price: null as never,
-          quantity: null as never,
-        }
-      },
-      set(value: Product): void {
-        this.$store.dispatch('addProduct', value)
-      },
-    },
+    ...mapState({ product: 'product' }),
     ...mapGetters({
       products: 'products',
       productTableHeaders: 'productTableHeaders',
-      total: 'total',
     }),
+    total() {
+      return this.products.reduce((total: number, product: Product) => {
+        return total + product.price * product.quantity
+      }, 0)
+    },
+  },
+  created() {
+    this.fetchAllProducts()
+  },
+  components: {
+    ValidationObserver,
+    ValidationProvider,
   },
 })
 </script>
-
-<style scoped></style>
